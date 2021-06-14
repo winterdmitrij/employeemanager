4b995ce5ee5a5e8bdf06b490fc63fe833b624f46
winterdmitrij <winterdmitrij@gmail.com>
2021-06-14 13:26:36 +0200

    Repository and Service

---
+++ src/main/java/de/winter/employeemanager/repository/EmployeeRepo.java
@@ -0,0 +1,15 @@
+package de.winter.employeemanager.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import de.winter.employeemanager.model.Employee;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long>{
+	void deleteEmployeeById(Long id);
+
+	Optional findEmployeeById(Long id);
+}
---
+++ src/main/java/de/winter/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package de.winter.employeemanager.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.winter.employeemanager.model.Employee;
+import de.winter.employeemanager.repository.EmployeeRepo;
+
+@Service
+public class EmployeeService {
+	private final EmployeeRepo employeeRepo;
+
+	@Autowired
+	public EmployeeService(EmployeeRepo employeeRepo) {
+		this.employeeRepo = employeeRepo;
+	}
+	
+	public Employee addEmployee(Employee employee) {
+		employee.setEmployeeCode(UUID.randomUUID().toString());
+		return employeeRepo.save(employee);
+	}
+	
+	public List<Employee> findAllEmployees() {
+		return employeeRepo.findAll();
+	}
+	
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepo.save(employee);
+	}
+	
+	public Employee findEmployeeById(Long id) {
+		return employeeRepo.findEmployeeById(id)
+				.orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+	}
+	
+	public void deleteEmploye(Long id) {
+		employeeRepo.deleteEmployeeById(id);
+	}
+}
